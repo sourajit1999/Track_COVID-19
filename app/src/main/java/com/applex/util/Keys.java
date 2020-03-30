a346b1f434048b99733c75b37df87d3418e4f482
ankita-de-222000 <ankita.de10@gmail.com>
2020-03-30 17:10:45 +0530

    keys added

--- app/src/main/java/com/applex/util/Keys.java
+++ app/src/main/java/com/applex/util/Keys.java
@@ -6,7 +6,7 @@ public class Keys {
 
     public static final String Sheet1_Script_id = "https://script.google.com/macros/s/AKfycbyhJIZS1Kud2f9yfxTfCWAgzm7yziMUg7_FetHFrwTsRkwo0HY/exec";
     public static final String Sheet1_Sheet_id = "1RutV395z_mRkRGaxb5OZsS5XyaWJm25_hFEgcf8C3QU";
-
+//https://script.google.com/macros/s/AKfycbyhJIZS1Kud2f9yfxTfCWAgzm7yziMUg7_FetHFrwTsRkwo0HY/exec
     public static final String Sheet2_Script_id = "https://script.google.com/macros/s/AKfycbzaglCeQamw8WRXzi5rSzZ6PNd9Nx8ZQtn2ACidHc0537vpKBKU/exec";
     public static final String Sheet2_Sheet_id = "1uzNPCS8l9iG7sajBSTTLiLcSxBEoHlWEu36t880ihyE";
 
@@ -15,5 +15,5 @@ public class Keys {
 
     public static final String Sheet4_Script_id = "https://script.google.com/macros/s/AKfycbw5Q9vOjJU0__5MLAU4wqott9rRM7tlpfHmQqUn-Zr1ZijCqYo/exec";
     public static final String Sheet4_Sheet_id = "1f5eX5L3a5Xb4Vi5amFH2d0hFgqWa-suHfh_m5XAjzTc";
-
+//https://script.google.com/macros/s/AKfycbw5Q9vOjJU0__5MLAU4wqott9rRM7tlpfHmQqUn-Zr1ZijCqYo/exec
 }
