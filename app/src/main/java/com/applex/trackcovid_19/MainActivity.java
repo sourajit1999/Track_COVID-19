2bf84d83aa4ad768347ea4d3ebca89dd1b229ee3
sourajit1999 <amisourajit123@gmail.com>
2020-03-30 18:36:16 +0530

    initial

--- app/src/main/java/com/applex/trackcovid_19/MainActivity.java
+++ app/src/main/java/com/applex/trackcovid_19/MainActivity.java
@@ -149,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
 
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            Intent intent = new Intent(getApplicationContext(), InputDetails.class);
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
                             startActivity(intent);
--- app/src/main/java/com/applex/trackcovid_19/Splash.java
+++ app/src/main/java/com/applex/trackcovid_19/Splash.java
@@ -17,7 +17,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(Splash.this, MainActivity.class);
+                Intent homeIntent = new Intent(Splash.this, HomePage.class);
                 startActivity(homeIntent);
                 finish();
             }
