3351bcdcc2774b7e043688899fa57c6cd9fa95d5
sourajit1999 <amisourajit123@gmail.com>
2020-03-30 18:27:28 +0530

    initial

--- app/src/main/java/com/applex/trackcovid_19/MainActivity.java
+++ app/src/main/java/com/applex/trackcovid_19/MainActivity.java
@@ -1,21 +1,41 @@
 package com.applex.trackcovid_19;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Boolean.FALSE;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner mSpinner;
     private EditText mPhoneNo;
     private EditText mPinCode;
+    private Spinner mSpinnerBlood;
+
+    Dialog mydialogue;
+    String verificationid;
 
 
     @Override
@@ -42,26 +62,108 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 String phonenumber = "+" + code + number;
-//
-//                Intent intent = new Intent(RegisterActivity.this, OtpActivity.class);
-//                intent.putExtra("phonenumber", phonenumber);
-//                startActivity(intent);
+                sendVerificationCode(phonenumber);
 
             }
         });
 
+
+
     }
+
+//    private void showDialog(){
+//        mydialogue = new Dialog(MainActivity.this);
+//
+//        mydialogue.setContentView(R.layout.jumping_panda_progress);
+//        mydialogue.setCanceledOnTouchOutside(FALSE);
+//        mydialogue.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//
+//        EditText otp;
+//        otp = mydialogue.findViewById(R.id.otp);
+//        otp.getText().
+//
+//        mydialogue.show();
+//
+//    }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-            startActivity(intent);
-        }
+//
+//        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+//            Intent intent = new Intent(this, MainActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//
+//            startActivity(intent);
+//        }
     }
+
+    /////////////////SEND NO FOR VERIFICATION/////////////
+    private void sendVerificationCode(String number){
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                number,
+                60,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallBack
+        );
+    }
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationid = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if (code != null){
+                mydialogue.dismiss();
+//                progressBar.setVisibility(View.VISIBLE);
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+
+        }
+    };
+    /////////////////SEND NO FOR VERIFICATION/////////////
+
+
+
+    private void verifyCode(String code){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationid, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+    }
+
+
+
+
 }
 
---
+++ app/src/main/java/com/applex/trackcovid_19/Splash.java
@@ -0,0 +1,26 @@
+package com.applex.trackcovid_19;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    private static final long Splash_time_out = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(Splash.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, Splash_time_out);
+    }
+}
