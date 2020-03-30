1eb19f819eb34b0e6c48b6b4b8d76a14e89e1640
ankita-de-222000 <ankita.de10@gmail.com>
2020-03-30 16:03:51 +0530

    keys added

---
+++ app/src/main/java/com/applex/util/Keys.java
@@ -0,0 +1,58 @@
+package com.applex.util;
+
+public class Keys {
+
+    //PERSONAL DETAILS//
+
+    public static final String name = "Name";
+    public static final String institution = "Institution";
+    public static final String designation = "Designation";
+    public static final String email = "email";
+    public static final String contact = "Contact_No";
+    public static final String source = "Source_of_Information";
+    public static final String participant_id = "Participant_Id";
+    public static final String idSheet = "id";
+
+    public static final String sheetID = "Sheet1";
+    //PERSONAL DETAILS//
+
+    //FIREBASE STORAGE//
+
+//    public static final String firebaseHomeRef = "gs://innovacion-b8c51.appspot.com/";
+
+    //FIREBASE STORAGE//
+
+    //TEAM DETAILS//
+    public static final String teamname = "teamname";
+    public static final String idcap = "idcap";
+    public static final String id1 = "id1";
+    public static final String id2 = "id2";
+    public static final String id3 = "id3";
+    public static final String id4 = "id4";
+    //TEAM DETAILS//
+
+
+    //FETCH TEAM DETAILS//
+
+    public static final String teamId = "Team_ID";
+    public static final String eventTeamName = "Team_Name";
+    public static final String eventEmail = "Email";
+
+
+    //FETCH TEAM DETAILS//
+
+
+
+    //SCRIPT & SHEET IDS//
+
+    public static final String Sheet1_Script_id = "https://script.google.com/macros/s/AKfycbyhJIZS1Kud2f9yfxTfCWAgzm7yziMUg7_FetHFrwTsRkwo0HY/exec";
+    public static final String Sheet1_Sheet_id = "1RutV395z_mRkRGaxb5OZsS5XyaWJm25_hFEgcf8C3QU";
+
+    public static final String Sheet2_Script_id = "https://script.google.com/macros/s/AKfycbzaglCeQamw8WRXzi5rSzZ6PNd9Nx8ZQtn2ACidHc0537vpKBKU/exec";
+    public static final String Sheet2_Sheet_id = "1uzNPCS8l9iG7sajBSTTLiLcSxBEoHlWEu36t880ihyE";
+
+    public static final String Sheet3_Script_id = "https://script.google.com/macros/s/AKfycbyskjQOOSHLHuASh8er1SsZB3zGupMbtfFKfaGZyLL69GsTi9c/exec";
+    public static final String Sheet3_Sheet_id = "1H4rtt5S3vXi-BDiCzGi5QKENJzKaLfgWC-CAVQgEhVE";
+
+
+}
