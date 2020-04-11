caa61a3f4343b5bc37854cda5706d711db01eb82
ankita-de-222000 <ankita.de10@gmail.com>
2020-04-11 22:50:57 +0530

    Medical registration Activity Added

--- app/src/main/java/com/applex/trackcovid_19/RegistrationFormPost.java
+++ app/src/main/java/com/applex/trackcovid_19/RegistrationFormPost.java
@@ -2,13 +2,40 @@ package com.applex.trackcovid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.applex.trackcovid_19.util.Utility;
 
 public class RegistrationFormPost extends AppCompatActivity {
+    EditText registrationID;
+    Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form_post);
+        registrationID=findViewById(R.id.reg_id);
+        go=findViewById(R.id.go);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+              final String input_id= registrationID.getText().toString().trim();
+              if(input_id.matches("asdf34")){
+                    Intent i=new Intent(getApplicationContext(),DisplayActivity.class);
+                    startActivity(i);
+                     overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                     finish();
+                }
+                else
+                    Utility.showToast(getApplicationContext(),"Enter a valid Registration ID!");
+            }
+        });
+
     }
 }
